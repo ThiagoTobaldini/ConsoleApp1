f01ef91f52faabd327a95caf968ea975bc0fff16
thiago tobaldini <thiagobispotobaldini@hotmail.com>
2018-06-12 15:26:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var interpreter = new Interpreter();
+            //double visor = 1;
+
+            //visor = visor + double.Parse(Console.ReadKey().KeyChar.ToString());
+            //Console.Clear();
+            //Console.WriteLine(visor.ToString("c2"));
+            //visor = visor + double.Parse(Console.ReadKey().KeyChar.ToString());
+            //Console.Clear();
+
+            DateTime TM1 = System.DateTime.Now;
+
+            TM1.ToShortDateString();
+
+
+
+
+
+            //var resultformatado = result.ToString("c2");
+            Console.WriteLine(TM1.ToShortDateString());
+            Console.ReadKey();
+        }
+    }
+}
